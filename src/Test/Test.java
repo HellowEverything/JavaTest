427f788ba577da0fa2170af888e0b6f5174512c2
zs <zs1240268299@163.com>
2018-12-24 16:17:15 +0800

    eclipse使用git案例

---
+++ src/Test/Test.java
@@ -0,0 +1,26 @@
+package Test;
+
+import java.awt.List;
+import java.util.Arrays;
+
+public class Test {
+				
+	public static void main(String[] args) {
+		String str1="";
+		System.out.println(str1);
+		String[] str2= {"a","b","c"};
+		List list=new List();
+		list.add("a");
+		list.add("b");
+		System.out.println(Arrays.toString(str2)+"\n"+list);
+//		for(String s: str2) {
+//			System.out.println(s);
+//		}
+//		for(int i=0;i<list.size();i++) {
+//			System.out.println(list.get(i) instanceof String);
+//		}
+
+	}
+	
+}
+
